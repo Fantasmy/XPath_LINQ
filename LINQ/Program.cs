5f5e4061623322b1c4395193a7cf43242ba5e64d
Jenny <fantazmit@gmail.com>
2018-07-01 22:12:37 +0200

    Linq will be in another project. Finished XPath. Close #1

---
+++ LINQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument xmlDocument = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("Creating an XML tree using LINQ to XML"),
+                new XElement("PurchaseOrder",
+                    new XElement("Address", new XAttribute("Type", "Shipping"),
+                        new XElement("Name", "Ellen Adams"),
+                        new XElement("Street", "123 Maple Street"),
+                        new XElement("City", "Mill Valley"),
+                        new XElement("State", "CA"),
+                        new XElement("Zip", "10999"),
+                        new XElement("Country", "USA")),
+                    new XElement("Address", new XAttribute("Type", "Billing"),
+                        new XElement("Name", "Tai Yee"),
+                        new XElement("Street", "8 Oak Avenue"),
+                        new XElement("City", "Old Town"),
+                        new XElement("State", "PA"),
+                        new XElement("Zip", "95819"),
+                        new XElement("Country", "USA"))));
+
+            xmlDocument.Save("linq.xml");
+        }
+    }
+}
